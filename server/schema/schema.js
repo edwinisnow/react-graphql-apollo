a38e59cdb3c82bf2aca9ff9a634e2082605a0fb4
edwinisnow <edwin04@gmail.com>
2019-11-02 06:59:36 +0000

    Type Relations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -14,9 +14,9 @@ const graphql = require('graphql')
 // const [GraphQLObjectType, GraphQLString] = require('graphql')
 
 var books = [
-    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-    { name: 'The final Empire', genre: 'Fantasy', id: '2' },
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorid: '1' },
+    { name: 'The final Empire', genre: 'Fantasy', id: '2', authorid: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorid: '3' },
 ]
 
 var authors = [
@@ -31,6 +31,13 @@ const BookType = new GraphQLObjectType({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args) {
+                console.log(parent)
+                return _.find(authors, { id: parent.authorid })
+            }
+        }
     })
 })
 
